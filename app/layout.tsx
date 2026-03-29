0a0db6d5883078e156ad3a6d70062fed0d85829a
Keyairius Hopkins <kcdhopkins@gmail.com>
2026-03-28 22:22:18 -0500

    fix: white text

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body >
+      <body className="text-black">
         <Header />
         {children}
       </body>
